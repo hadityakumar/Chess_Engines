4dfc188ff7a773b117bc0a220c8bf85e9fefd222
hadityakumar <hadityakumar@gmail.com>
2024-07-08 11:24:46 +0530

    responsiveness fixes

--- app/random/page.js
+++ app/random/page.js
@@ -94,7 +94,7 @@ const App = () => {
         <>
             <div className="flex flex-col md:flex-row font-[montserrat] font-extrabold justify-between items-center md:items-start p-5">
                 <div className='w-full md:w-[30%] flex flex-col items-center p-5 gap-5'>
-                    <span className='text-black md:text-xl text-base text-center'>Hello I am Random AI!</span>
+                    <span className='text-black md:text-xl text-base text-center'>Hello I am Random AI !</span>
                     <span className='text-black md:text-lg text-sm text-center'>I play random moves</span>
                     <span className='text-gray-700 md:text-lg text-base text-center'>How I work?</span>
                     <span className='text-gray-700 md:text-base text-sm text-center'>I find all possible legal moves at a particular position and choose any one of them using RNG (Random Number Generator)</span>
